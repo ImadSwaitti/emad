c171b30b16daf4d88f1583e218591a73facd8bb3
ImadSwaitti <202202177@bethlehem.edu>
2024-10-28 12:30:03 +0200

    Add read Method

--- src/main/java/emad/App.java
+++ src/main/java/emad/App.java
@@ -1,13 +1,33 @@
 package emad;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static List<Sale> listOfsales;
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
+        listOfsales = readSales("sales.csv");
+    }
+
+
+       public static List<Sale> readSales(String filePath) throws IOException {
+        return Files.lines(Paths.get(filePath))
+                .skip(1)
+                .map(line -> {
+                    String[] row = line.split(",");
+                    return new Sale(Integer.parseInt(row[0]), row[1], row[2], row[3], Integer.parseInt(row[4]),
+                            Integer.parseInt(row[5]), Integer.parseInt(row[6]), LocalDate.parse(row[7]));
+                })
+                .collect(Collectors.toList());
     }
 }
